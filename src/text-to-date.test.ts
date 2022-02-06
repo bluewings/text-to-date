7d7e6190c84d836b997b9b118ff321263fb47c59
sungwon-cha <mygenie3@naver.com>
2022-02-06 10:33:55 +0900

    fix

--- src/text-to-date.test.ts
+++ src/text-to-date.test.ts
@@ -1,7 +1,143 @@
+import {
+  addDays,
+  addMonths,
+  addQuarters,
+  addYears,
+  endOfMonth,
+  endOfQuarter,
+  endOfWeek,
+  endOfYear,
+  setDay,
+  startOfDay,
+  startOfMonth,
+  startOfQuarter,
+  startOfWeek,
+  startOfYear,
+} from 'date-fns';
 import { textToDate } from './text-to-date';
 
 describe('default', () => {
   test('yyyy-MM-dd', () => {
     expect(textToDate('2022-01-01')).toEqual(new Date(`2022-01-01T00:00:00.000Z`));
+  });
+
+  test('start of this week', () => {
+    expect(textToDate('start of this week')).toEqual(startOfWeek(new Date()));
+  });
+
+  test('end of this week', () => {
+    expect(textToDate('end of this week')).toEqual(endOfWeek(new Date()));
+  });
+
+  test('start of last week', () => {
+    expect(textToDate('start of last week')).toEqual(startOfWeek(addDays(new Date(), -7)));
+  });
+
+  test('end of last week', () => {
+    expect(textToDate('end of last week')).toEqual(endOfWeek(addDays(new Date(), -7)));
+  });
+
+  test('start of next week', () => {
+    expect(textToDate('start of next week')).toEqual(startOfWeek(addDays(new Date(), 7)));
+  });
+
+  test('end of next week', () => {
+    expect(textToDate('end of next week')).toEqual(endOfWeek(addDays(new Date(), 7)));
+  });
+
+  test('start of this month', () => {
+    expect(textToDate('start of this month')).toEqual(startOfMonth(new Date()));
+  });
+
+  test('end of this month', () => {
+    expect(textToDate('end of this month')).toEqual(endOfMonth(new Date()));
+  });
+
+  test('start of last month', () => {
+    expect(textToDate('start of last month')).toEqual(startOfMonth(addMonths(new Date(), -1)));
+  });
+
+  test('end of last month', () => {
+    expect(textToDate('end of last month')).toEqual(endOfMonth(addMonths(new Date(), -1)));
+  });
+
+  test('start of next month', () => {
+    expect(textToDate('start of next month')).toEqual(startOfMonth(addMonths(new Date(), 1)));
+  });
+
+  test('end of next month', () => {
+    expect(textToDate('end of next month')).toEqual(endOfMonth(addMonths(new Date(), 1)));
+  });
+
+  test('start of this year', () => {
+    expect(textToDate('start of this year')).toEqual(startOfYear(new Date()));
+  });
+
+  test('end of this year', () => {
+    expect(textToDate('end of this year')).toEqual(endOfYear(new Date()));
+  });
+
+  test('start of last year', () => {
+    expect(textToDate('start of last year')).toEqual(startOfYear(addYears(new Date(), -1)));
+  });
+
+  test('end of last year', () => {
+    expect(textToDate('end of last year')).toEqual(endOfYear(addYears(new Date(), -1)));
+  });
+
+  test('start of next year', () => {
+    expect(textToDate('start of next year')).toEqual(startOfYear(addYears(new Date(), 1)));
+  });
+
+  test('end of next year', () => {
+    expect(textToDate('end of next year')).toEqual(endOfYear(addYears(new Date(), 1)));
+  });
+
+  test('start of this quarter', () => {
+    expect(textToDate('start of this quarter')).toEqual(startOfQuarter(new Date()));
+  });
+
+  test('end of this quarter', () => {
+    expect(textToDate('end of this quarter')).toEqual(endOfQuarter(new Date()));
+  });
+
+  test('start of last quarter', () => {
+    expect(textToDate('start of last quarter')).toEqual(startOfQuarter(addQuarters(new Date(), -1)));
+  });
+
+  test('end of last quarter', () => {
+    expect(textToDate('end of last quarter')).toEqual(endOfQuarter(addQuarters(new Date(), -1)));
+  });
+
+  test('start of next quarter', () => {
+    expect(textToDate('start of next quarter')).toEqual(startOfQuarter(addQuarters(new Date(), 1)));
+  });
+
+  test('end of next quarter', () => {
+    expect(textToDate('end of next quarter')).toEqual(endOfQuarter(addQuarters(new Date(), 1)));
+  });
+
+  test('this sunday', () => {
+    expect(textToDate('this sunday')).toEqual(startOfDay(setDay(new Date(), 0)));
+  });
+
+  test('this saturday', () => {
+    expect(textToDate('this saturday')).toEqual(startOfDay(setDay(new Date(), 6)));
+  });
+
+  test('last sunday', () => {
+    expect(textToDate('last sunday')).toEqual(startOfDay(setDay(addDays(new Date(), -7), 0)));
+  });
+
+  test('last saturday', () => {
+    expect(textToDate('last saturday')).toEqual(startOfDay(setDay(addDays(new Date(), -7), 6)));
+  });
+
+  test('next sunday', () => {
+    expect(textToDate('next sunday')).toEqual(startOfDay(setDay(addDays(new Date(), 7), 0)));
+  });
+
+  test('next saturday', () => {
+    expect(textToDate('next saturday')).toEqual(startOfDay(setDay(addDays(new Date(), 7), 6)));
   });
 });
--- src/text-to-date.ts
+++ src/text-to-date.ts
@@ -1,3 +1,207 @@
-export const textToDate = (text: string) => {
-  return new Date(text);
+import {
+  addDays,
+  addMonths,
+  addQuarters,
+  addYears,
+  endOfMonth,
+  endOfQuarter,
+  endOfWeek,
+  endOfYear,
+  setDay,
+  startOfDay,
+  startOfMonth,
+  startOfQuarter,
+  startOfWeek,
+  startOfYear,
+} from 'date-fns';
+
+enum TextToken {
+  StartOf = 'startof',
+  EndOf = 'endof',
+  Monday = 'mon',
+  Tuesday = 'tue',
+  Wednesday = 'wed',
+  Thursday = 'thu',
+  Friday = 'fri',
+  Saturday = 'sat',
+  Sunday = 'sun',
+  ThisWeek = 'thisweek',
+  LastWeek = 'lastweek',
+  NextWeek = 'nextweek',
+  ThisMonth = 'thismonth',
+  LastMonth = 'lastmonth',
+  NextMonth = 'nextmonth',
+  ThisYear = 'thisyear',
+  LastYear = 'lastyear',
+  NextYear = 'nextyear',
+  ThisQuarter = 'thisquarter',
+  LastQuarter = 'lastquarter',
+  NextQuarter = 'nextquarter',
+}
+
+const tokens = {
+  [TextToken.StartOf]: ['start of'],
+  [TextToken.EndOf]: ['end of'],
+  [TextToken.Monday]: ['monday'],
+  [TextToken.Tuesday]: ['tuesday'],
+  [TextToken.Wednesday]: ['wednesday'],
+  [TextToken.Thursday]: ['thursday'],
+  [TextToken.Friday]: ['friday'],
+  [TextToken.Saturday]: ['saturday'],
+  [TextToken.Sunday]: ['sunday'],
+  [TextToken.ThisWeek]: ['this week'],
+  [TextToken.LastWeek]: ['last week'],
+  [TextToken.NextWeek]: ['next week'],
+  [TextToken.ThisMonth]: ['this month'],
+  [TextToken.LastMonth]: ['last month'],
+  [TextToken.NextMonth]: ['next month'],
+  [TextToken.ThisYear]: ['this year'],
+  [TextToken.LastYear]: ['last year'],
+  [TextToken.NextYear]: ['next year'],
+  [TextToken.ThisQuarter]: ['this quarter'],
+  [TextToken.LastQuarter]: ['last quarter'],
+  [TextToken.NextQuarter]: ['next quarter'],
 };
+
+const b____b = Object.entries(tokens)
+  .reduce(
+    (accum, [replace, terms]) => [...accum, ...terms.map((find) => ({ find, replace }))],
+    [] as { find: string; replace: string }[],
+  )
+  .sort((a, b) => (a.find.length === b.find.length ? 0 : a.find.length > b.find.length ? -1 : 1));
+
+const daysInWeek: string[] = [
+  TextToken.Sunday,
+  TextToken.Monday,
+  TextToken.Tuesday,
+  TextToken.Wednesday,
+  TextToken.Thursday,
+  TextToken.Friday,
+  TextToken.Saturday,
+];
+
+export const textToDate = (text: string) => {
+  const source = text.toLowerCase().replace(/\s+/g, ' ');
+
+  // basic conversion
+  const date = new Date(source);
+  if (isValidDate(date)) {
+    return new Date(source);
+  }
+
+  // console.log(bb);
+
+  // source.replace()
+  const ccc = b____b
+    .reduce((accu, e) => {
+      return accu.replace(e.find, e.replace);
+    }, source)
+    .split(' ');
+
+  const today = new Date();
+
+  if (ccc.includes(TextToken.StartOf) || ccc.includes(TextToken.EndOf)) {
+    let stdDate = new Date();
+    let isStart = ccc.includes(TextToken.StartOf);
+    let fn;
+
+    if (ccc.includes(TextToken.ThisWeek)) {
+      // stdDate = addDays(today, -7);
+      stdDate = today;
+      fn = isStart ? startOfWeek : endOfWeek;
+    } else if (ccc.includes(TextToken.LastWeek)) {
+      stdDate = addDays(today, -7);
+      fn = isStart ? startOfWeek : endOfWeek;
+    } else if (ccc.includes(TextToken.NextWeek)) {
+      stdDate = addDays(today, 7);
+      fn = isStart ? startOfWeek : endOfWeek;
+    } else if (ccc.includes(TextToken.ThisMonth)) {
+      stdDate = today;
+      fn = isStart ? startOfMonth : endOfMonth;
+    } else if (ccc.includes(TextToken.LastMonth)) {
+      stdDate = addMonths(today, -1);
+      fn = isStart ? startOfMonth : endOfMonth;
+    } else if (ccc.includes(TextToken.NextMonth)) {
+      stdDate = addMonths(today, 1);
+      fn = isStart ? startOfMonth : endOfMonth;
+    } else if (ccc.includes(TextToken.ThisYear)) {
+      stdDate = today;
+      fn = isStart ? startOfYear : endOfYear;
+    } else if (ccc.includes(TextToken.LastYear)) {
+      stdDate = addYears(today, -1);
+      fn = isStart ? startOfYear : endOfYear;
+    } else if (ccc.includes(TextToken.NextYear)) {
+      stdDate = addYears(today, 1);
+      fn = isStart ? startOfYear : endOfYear;
+    } else if (ccc.includes(TextToken.ThisQuarter)) {
+      stdDate = today;
+      fn = isStart ? startOfQuarter : endOfQuarter;
+    } else if (ccc.includes(TextToken.LastQuarter)) {
+      stdDate = addQuarters(today, -1);
+      fn = isStart ? startOfQuarter : endOfQuarter;
+    } else if (ccc.includes(TextToken.NextQuarter)) {
+      stdDate = addQuarters(today, 1);
+      fn = isStart ? startOfQuarter : endOfQuarter;
+    }
+
+    if (fn) {
+      return fn(stdDate);
+    }
+    // }
+
+    // if (ccc.includes(TextToken.StartOf)) {
+    //   if (ccc.includes(TextToken.ThisWeek)) {
+    //     return startOfWeek(today);
+    //   } else if (ccc.includes(TextToken.LastWeek)) {
+    //     return startOfWeek(addDays(today, -7));
+    //   } else if (ccc.includes(TextToken.NextWeek)) {
+    //     return startOfWeek(addDays(today, 7));
+    //   } else if (ccc.includes(TextToken.ThisMonth)) {
+    //     return startOfMonth(today);
+    //   } else if (ccc.includes(TextToken.LastMonth)) {
+    //     return startOfMonth(addMonths(today, -1));
+    //   } else if (ccc.includes(TextToken.NextMonth)) {
+    //     return startOfMonth(addMonths(today, 1));
+    //   }
+    // } else if (ccc.includes(TextToken.EndOf)) {
+    //   if (ccc.includes(TextToken.ThisWeek)) {
+    //     return endOfWeek(today);
+    //   } else if (ccc.includes(TextToken.LastWeek)) {
+    //     return endOfWeek(addDays(today, -7));
+    //   } else if (ccc.includes(TextToken.NextWeek)) {
+    //     return endOfWeek(addDays(today, 7));
+    //   }
+  } else {
+    // const day = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+    const aaa = ccc.find((e) => daysInWeek.includes(e));
+    const dayofw = aaa ? daysInWeek.indexOf(aaa) : -1;
+    // console.log(ccc, dayofw);
+
+    // if (dayofw_) {
+
+    // }
+
+    if (dayofw !== -1) {
+      let week = new Date();
+      if (ccc.includes('last')) {
+        week = addDays(week, -7);
+      } else if (ccc.includes('next')) {
+        week = addDays(week, 7);
+      }
+      return startOfDay(setDay(week, dayofw));
+    }
+
+    // if (ccc.includes('this')) {
+    // }
+  }
+
+  // source.
+
+  // source.match()
+
+  return null;
+};
+
+const isValidDate = (date: Date) => !Number.isNaN(date.getTime());
+
+// 37라 7818
